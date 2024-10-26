7274d640134904491bf195620a530e1c11a0f50b
Ma-Thandar <mamatda@gmail.com>
2024-10-26 13:55:37 +0630

    Update streamlit_demo.py

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -1,11 +1,26 @@
 import streamlit as st
 from datetime import date
 
+st.set_page_config(layout="wide")
+
+
+st.title("Hello")
+col1, col2, col3  = st.columns(3) 
+with st.expander(" see more "):
+
+    st.selectbox(
+        "Select a topic of your interest",
+        ["arts", "automobiles", "books", "business", "fashion", "food", "health", "home",
+         "insider", "magazine", "movies", "nyregion", "obituaries", "opinion", "politics",
+         "realestate", "science", "sports", "sundayreview", "technology", "theater", "t-magazine",
+         "travel", "upshot", "us", "world"]
+    )
+
 st.title("Sum Odd numbers")
 #st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 #number = st.number_input("Insert a number")
 #number = st.number_input("Insert a number", value=None, placeholder="Type a number...")
-number = st.number_input("Insert a number") 
+number = st.number_input("Insert a number", min_value=1, max_value=100, value="min") 
 #st.write("The current number is ", number)
 i=1; n=1; s=0;
 while i<=number:
@@ -24,7 +39,10 @@ st.write("Sum of 1st 10 odd numbers ",number*number)
 
 
 st.title("Calculate Age")
-d = st.date_input("When's your birthday",min_value=1, max_value=100)
+minDate = date(1900,1,1)
+maxDate = date(2120,1,1)
+
+d = st.date_input("When's your birthday",date.today(),minDate,maxDate)
 st.write("Your birthday is:", d)
 def age_calculator(d):
     today = date.today()
@@ -41,5 +59,4 @@ def age_calculator(d):
     return f'You are {years} years, {months} months and {int(days)} days old.'
 
 age = age_calculator(d)
-st.write("You are now ",age)    
-
+st.write("You are now ",age)  
